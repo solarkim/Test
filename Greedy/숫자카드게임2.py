9799ae95bf679f89bbb4955452a4cb6f796d4891
solarkim <ty7613@naver.com>
2021-07-17 16:28:04 +0900

    commit

--- Greedy/숫자카드게임2.py
+++ Greedy/숫자카드게임2.py
@@ -5,12 +5,16 @@
 # 따라서 처음에 카드를 골라낼 행을 선택할 때, 이후에 해당 행에서 가장 숫자가 낮은 카드를 뽑을 것을 고려하여
 # 최종적으로 가장 높은 숫자의 카드를 뽑을 수 있또록 전략을 세워야 한다.
 
+
+# 이중 반복문 활용
 n,m = map(int, input().split())
 
 result=0
 for i in range(n):
     data = list(map(int,input().split()))
-    min_value=min(data)
+    min_value = 10001
+    for a in data:
+        min_value=min(min_value,a)
     reslut=max(result,min_value)
 
 print(result)
